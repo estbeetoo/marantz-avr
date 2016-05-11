7e1a9ce88043494379708873f94e776b4d88d412
aborovsky <kycok.6o6pa@gmail.com>
2016-05-11 20:00:01 +0300

    Test improved: now testing setVolumeLevel and setVolumePercent

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var should = require('should');
 var AVReciever = require('../lib/avreciever');
 
 describe('AVReciever', function() {
-  var reciever = new AVReciever('192.168.2.106');
+  var reciever = new AVReciever('192.168.2.139');
 
   this.timeout(10000);
 
@@ -62,5 +62,137 @@ describe('AVReciever', function() {
         });
     });
 
+  });
+
+  describe('#setVolumeLevel', function() {
+    it('should set volume to -80.0', function() {
+      return reciever.setVolumeLevel(-80)
+        .then(function() {
+          return reciever.getVolumeLevel().then(function(volumeLevel) {
+            parseFloat(volumeLevel).should.be.equal(-80.0);
+          });
+        });
+    });
+    it('should set volume to -60.0', function() {
+      return reciever.setVolumeLevel(-60)
+        .then(new Promise(function(resolve, reject) {
+          setTimeout(function() {
+            reciever.getVolumeLevel().then(function(volumeLevel) {
+              parseFloat(volumeLevel).should.be.equal(-60.0);
+              resolve()
+            }, reject);
+          });
+        }));
+    });
+    it('should set volume to -31.0', function() {
+      return reciever.setVolumeLevel(-31)
+        .then(new Promise(function(resolve, reject) {
+          setTimeout(function() {
+            reciever.getVolumeLevel().then(function(volumeLevel) {
+              parseFloat(volumeLevel).should.be.equal(-31.0);
+              resolve()
+            }, reject);
+          });
+        }));
+    });
+    it('should set volume to -11.0', function() {
+      return reciever.setVolumeLevel(-11)
+        .then(new Promise(function(resolve, reject) {
+          setTimeout(function() {
+            reciever.getVolumeLevel().then(function(volumeLevel) {
+              parseFloat(volumeLevel).should.be.equal(-11.0);
+              resolve()
+            }, reject);
+          });
+        }));
+    });
+    it('should set volume to 18.0', function() {
+      return reciever.setVolumeLevel(18)
+        .then(new Promise(function(resolve, reject) {
+          setTimeout(function() {
+            reciever.getVolumeLevel().then(function(volumeLevel) {
+              parseFloat(volumeLevel).should.be.equal(18.0);
+              resolve()
+            }, reject);
+          });
+        }));
+    });
+    it('should set volume to -80.0', function() {
+      return reciever.setVolumeLevel(-80)
+        .then(new Promise(function(resolve, reject) {
+          setTimeout(function() {
+            reciever.getVolumeLevel().then(function(volumeLevel) {
+              parseFloat(volumeLevel).should.be.equal(-80.0);
+              resolve()
+            }, reject);
+          });
+        }));
+    });
+  });
+
+  describe('#setVolumeLevelPercent', function() {
+    it('should set volume to 0%', function() {
+      return reciever.setVolumeLevelPercent(0)
+        .then(function() {
+          return reciever.getVolumeLevel().then(function(volumeLevel) {
+            parseFloat(volumeLevel).should.be.equal(-80.0);
+          });
+        });
+    });
+    it('should set volume to 20%', function() {
+      return reciever.setVolumeLevelPercent(20)
+        .then(new Promise(function(resolve, reject) {
+          setTimeout(function() {
+            reciever.getVolumeLevel().then(function(volumeLevel) {
+              parseFloat(volumeLevel).should.be.equal(-60.0);
+              resolve()
+            }, reject);
+          });
+        }));
+    });
+    it('should set volume to 50%', function() {
+      return reciever.setVolumeLevelPercent(50)
+        .then(new Promise(function(resolve, reject) {
+          setTimeout(function() {
+            reciever.getVolumeLevel().then(function(volumeLevel) {
+              parseFloat(volumeLevel).should.be.equal(-31.0);
+              resolve()
+            }, reject);
+          });
+        }));
+    });
+    it('should set volume to 70%', function() {
+      return reciever.setVolumeLevelPercent(70)
+        .then(new Promise(function(resolve, reject) {
+          setTimeout(function() {
+            reciever.getVolumeLevel().then(function(volumeLevel) {
+              parseFloat(volumeLevel).should.be.equal(-11.0);
+              resolve()
+            }, reject);
+          });
+        }));
+    });
+    it('should set volume to 100%', function() {
+      return reciever.setVolumeLevelPercent(100)
+        .then(new Promise(function(resolve, reject) {
+          setTimeout(function() {
+            reciever.getVolumeLevel().then(function(volumeLevel) {
+              parseFloat(volumeLevel).should.be.equal(18.0);
+              resolve()
+            }, reject);
+          });
+        }));
+    });
+    it('should set volume to 0%', function() {
+      return reciever.setVolumeLevelPercent(0)
+        .then(new Promise(function(resolve, reject) {
+          setTimeout(function() {
+            reciever.getVolumeLevel().then(function(volumeLevel) {
+              parseFloat(volumeLevel).should.be.equal(-80.0);
+              resolve()
+            }, reject);
+          });
+        }));
+    });
   });
 });
