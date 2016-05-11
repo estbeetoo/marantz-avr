37119f667b1e51e3d0febf8b9e91e5565e178caa
aborovsky <kycok.6o6pa@gmail.com>
2016-05-11 16:59:45 +0300

    power state handling fix + tests fixed

--- test/test.js
+++ test/test.js
@@ -1,62 +1,65 @@
 var should = require('should');
 var AVReciever = require('../lib/avreciever');
 
-describe('AVReciever', function(){
-  var reciever = new AVReciever('10.0.1.3');
+describe('AVReciever', function() {
+  var reciever = new AVReciever('192.168.2.106');
+
+  this.timeout(10000);
 
   beforeEach(function() {
-    reciever.setPowerState(true);
+    return reciever.setPowerState(true);
   });
 
-  describe('#getStatus', function(){
-    it('should return valid status', function(){
+  describe('#getStatus', function() {
+
+    it('should return valid status', function() {
       return reciever.getState()
-      .then(function(status) {
-        status.should.have.property('power');
-        status.should.have.property('mute');
-      });
+        .then(function(status) {
+          status.should.have.property('power');
+          status.should.have.property('mute');
+        });
     })
   });
 
   describe('power functions', function() {
     it('should power off', function() {
-      this.timeout(4000)
+      this.timeout(15000);
       return reciever.setPowerState(false)
-      .then(function() {
-        return reciever.getPowerState();
-      }).then(function(state) {
-        state.should.be.false
-      });
+        .then(function() {
+          return reciever.getPowerState().then(function(state) {
+            state.should.be.false
+          });
+        })
     })
   });
 
-  describe('#getStateFor', function(){
-    it('should return state for mute', function(){
+  describe('#getStateFor', function() {
+    it('should return state for mute', function() {
       return reciever.getStateFor('mute')
-      .then(function(state) {
-        (state !== undefined).should.be.ok;
-      });
+        .then(function(state) {
+          (state !== undefined).should.be.ok;
+        });
     })
   });
 
-  describe('#setMuteState', function(){
-    
+  describe('#setMuteState', function() {
+
     it('should set mute to true', function() {
       return reciever.setMuteState(true)
-      .then(function() {
-        return reciever.getMuteState();
-      }).then(function(muteState) {
-        muteState.should.be.true;
-      });
+        .then(function() {
+          return reciever.getMuteState();
+        }).then(function(muteState) {
+          muteState.should.be.true;
+        });
     });
 
-    it('should set mute to false', function(){
+    it('should set mute to false', function() {
       return reciever.setMuteState(false)
-      .then(function(status) {
-        return reciever.getMuteState();
-      }).then(function(muteState) {
-        muteState.should.be.false;
-      });
+        .then(function(status) {
+          return reciever.getMuteState();
+        }).then(function(muteState) {
+          muteState.should.be.false;
+        });
     });
 
   });
